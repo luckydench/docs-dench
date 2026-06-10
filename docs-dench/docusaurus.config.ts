f271461e92a360fd0a85d1e79c2d6f26a04c140f
Jung TaeSeung <mike415415@naver.com>
2026-06-10 22:16:02 +0900

    feat : 필요한 페이지 추가

--- docs-dench/docusaurus.config.ts
+++ docs-dench/docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/luckydench/docs-dench/tree/master/docs-dench/',
         },
         blog: {
           showReadingTime: true,
