f9ca467e89d2b2916460d0cb89c4e846ded992c6
Jung TaeSeung <mike415415@naver.com>
2026-06-11 15:10:39 +0900

    restruct : 폴더 구조 라우팅 구조 변경

--- docs-dench/docusaurus.config.ts
+++ docs-dench/docusaurus.config.ts
@@ -100,7 +100,6 @@ const config: Config = {
           type : "localeDropdown",
           position : 'right',
         },
-        { to: '/dench/graphql', label: 'DenchGraphQL', position: 'left' },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/luckydench/dench-fetch',
